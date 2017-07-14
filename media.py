eab3f4b9534688a2af7b53ec7580cae762037391
Erik Helman <erik.helman@outlook.com>
2017-07-14 08:46:00 -0400

    Minor style changes

--- media.py
+++ media.py
@@ -2,9 +2,13 @@
 
 import webbrowser
 
+
 class Movie():
+    # The Movie class contains all the required information for the movies to display on the final web page
 
     def __init__(self, movie_title, movie_storyline, movie_poster, movie_trailer, movie_director, movie_release_date):
+        # Initialize the movie with the required information
+
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster
@@ -12,7 +16,8 @@ class Movie():
         self.director = movie_director
         self.release_date = movie_release_date
 
-    #Open a browser and play the trailer
     def play_trailer(self):
+        # Play the trailer for the movie in the browser
+
         webbrowser.open(self.trailer)
 
